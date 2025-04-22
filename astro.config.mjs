d96d86fc51749eff546af9b959304d5854c4c86f
Iwo Plaza <iwoplaza@gmail.com>
2025-04-22 20:55:33 +0200

    Optimized some of the images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import playformCompress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [playformCompress()]
 });
